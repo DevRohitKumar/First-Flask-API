93cc4facd634e3d6485efcb43797ae718f2ebc34
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2023-02-09 16:48:36 +0530

    user delete good to go

--- simple_flask_api/routes.py
+++ simple_flask_api/routes.py
@@ -1,5 +1,6 @@
 from simple_flask_api import app, mysql
 from flask import jsonify, request
+from werkzeug.datastructures import ImmutableMultiDict
 
 @app.route('/api/users', methods=['GET'])
 def fetch_all_users():
@@ -7,32 +8,28 @@ def fetch_all_users():
         con_cur = mysql.connection.cursor()
         con_cur.execute("SELECT * FROM users")
         result = con_cur.fetchall()
+        con_cur.close()
         if result:
             return jsonify({"data": result}), 200
         else:
             return jsonify({"message": "No data found"}), 404
     except Exception as e:
         print(e)
-    finally:
-        con_cur.close()
-        
-
+                
 @app.route('/api/user/<int:id>', methods=['GET'])
 def fetch_single_user(id):
-    try:
+    try: 
         con_cur = mysql.connection.cursor()
         con_cur.execute("SELECT * FROM users WHERE id = {}".format(id))
-        result = con_cur.fetchone()
+        result = con_cur.fetchone()            
+        con_cur.close()      
         if result:
             return jsonify({"data": result}), 200
         else:
             return jsonify({"error": "No related data found ☹️"}), 404
     except Exception as e:
         print(e)
-    finally:
-        con_cur.close()      
         
-
 @app.route('/api/create', methods=['POST'])
 def create_user():
     try:        
@@ -47,22 +44,39 @@ def create_user():
             country_code = data['country_code']
             
             if fname and lname and email and gender and phone and country_code:
-                
-                conn_cursor = mysql.connection.cursor()
-                conn_cursor.execute("""INSERT INTO users (
+                con_cursor = mysql.connection.cursor()
+                con_cursor.execute("""INSERT INTO users (
                     fname, lname, gender, email, phone_number, phone_country_code)
                     VALUES (%s, %s,%s,%s,%s,%s)""", (fname, lname, gender, email, phone, country_code))
                 mysql.connection.commit()
-                conn_cursor.close()
-                
-                return jsonify({"message": "User created successfully 🥳 "}), 201              
-                
+                con_cursor.close()
+                return jsonify({"message": "User created successfully 🥳 "}), 201 
             else:
                 return jsonify({"error": "Some data is missing 🙁 "}), 206
-              
         else:
             return jsonify({"error": "Request body must be JSON ☠️ "}), 400
         
     except Exception as e:
         return jsonify({"error": "Something went wrong ☠️ "}), 400
         print(e)
+        
+@app.route('/api/delete/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    try:
+        # Check if user exist
+        con_cur = mysql.connection.cursor()
+        con_cur.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user_exist = con_cur.fetchone()  
+        
+        # return "WORKING !"
+        if user_exist and request.method == 'DELETE':
+            con_cur.execute("DELETE FROM users WHERE id = %s", (id,))
+            mysql.connection.commit()
+            return jsonify({"message" : "User deleted successfully"}), 200
+        else:
+            return jsonify({"error": "Something went wrong"}), 400
+    except Exception as e:
+        print(e)
+    finally:
+        con_cur.close()
+        
